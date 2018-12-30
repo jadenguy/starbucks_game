bfbcfa330941f4bd01626c22caa9f45ab1b58a79
Alfredo Inostroza <jadenguy@gmail.com>
2018-12-30 13:50:55 -0500

    Fixed winner bug Winners only worked when they were column 0 This is because the value of the winning bucket was init to 0 And to decide on the winning column, we looked at the column's value, not index

--- starbucks_game.py
+++ starbucks_game.py
@@ -1,57 +1,67 @@
 # https://medium.com/@ageitgey/learn-how-to-use-static-type-checking-in-python-3-6-in-10-minutes-12c86d72677b
-from random import gauss
 import random
 from typing import List
 from typing import Dict
 
 
-class group(object):
-    def __init__(self, name: str, columns: int, winPool: int):
+class badgeBoard(object):
+    def __init__(self, name: str, columns: int, winPool: int, seed: float):
+        self.r = random.Random(seed)
         self.columnCount: int = columns
         values: List[int] = list(range(columns))
         self.columns: Dict[int, int] = dict.fromkeys(values, 0)
-        self.winner: int = random.choice(values)
+        self.winner: int = self.r.choice(values)
         values.remove(self.winner)
         self.losers: List[int] = values
         self.winPool = winPool
 
     def __repr__(self) -> str:
         ret: str = ""
-        columnBadge: str = ""
         for x in range(self.columnCount):
-            if (self.columns[x] > 0):
+            columnBadge: str = "_"
+            if x == self.winner:
+                columnBadge = "*"
+            if (self.columns[x] != 0):
+                # columnBadge = str(self.columns[x])
                 columnBadge = "X"
-            else:
-                columnBadge = "_"
             ret += "[%s]" % (columnBadge)
         return ret
 
     def finished(self)-> bool:
-        badges: int = sum(self.columns.values())
-        fullSheet: bool = (badges == self.columnCount)
+        badges: int = min(self.columns.values())
+        fullSheet: bool = (badges != 0)
         return fullSheet
 
     def play(self)-> int:
-        selection: int = random.randint(1, self.winPool)
+        selection: int = self.r.randint(1, self.winPool)
         amIAWinner: bool = (selection % self.winPool == 0)
         ret: int = 0
         if (amIAWinner):
-            pickColumn = self.columns[self.winner]
+            pickColumn = self.winner
         else:
-            pickColumn = random.choice(self.losers)
-        if (self.columns[pickColumn] == 0):
-            self.columns[pickColumn] = 1
-        else:
+            pickColumn = self.r.choice(self.losers)
+        if (self.columns[pickColumn] != 0):
             ret = 1
+        self.columns[pickColumn] += 1
         return ret
 
 
-game = group('game', 10, 3)
-i = 0
-keepPlaying = (not game.finished())
+seed: float = random.random()
+game: badgeBoard = badgeBoard('game', 50, 1000, seed)
+i: int = 0
+freePlay: int = 0
+freePlayCounter: int = 0
+keepPlaying: bool = True
+print(game, i, freePlayCounter)
 while (keepPlaying):
-    # while (game.finished == False or i == 1000):
-    if(game.play()==0):
-        print(game, i)
     i += 1
-    keepPlaying = (not game.finished() and i < 100000)
+    if(game.play() == 0):
+        print(game, i, freePlayCounter)
+        pass
+    else:
+        freePlay += 1
+        if (freePlay == 3):
+            i -= 1
+            freePlay = 0
+            freePlayCounter += 1
+    keepPlaying = (not game.finished())
