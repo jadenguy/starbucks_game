01f0d9b09a1d128fb621fb9b26fe0db2f6645ccf
Alfredo Inostroza <jadenguy@gmail.com>
2018-12-29 21:25:05 -0500

    why doesn't this work like i expect? seems like one choice is just always harder to pin down maybe random.choice is nto that random without using the same random global variable

--- starbucks_game.py
+++ starbucks_game.py
@@ -1,25 +1,57 @@
+# https://medium.com/@ageitgey/learn-how-to-use-static-type-checking-in-python-3-6-in-10-minutes-12c86d72677b
 from random import gauss
 import random
 from typing import List
+from typing import Dict
 
 
 class group(object):
-    def __init__(self, name: str):
-        values: List[int] = list(range(3))
+    def __init__(self, name: str, columns: int, winPool: int):
+        self.columnCount: int = columns
+        values: List[int] = list(range(columns))
+        self.columns: Dict[int, int] = dict.fromkeys(values, 0)
         self.winner: int = random.choice(values)
         values.remove(self.winner)
         self.losers: List[int] = values
-        self.score: List[str] = []
-        for x in range(3):
-            self.score.append("_")
+        self.winPool = winPool
 
-    def __repr__(self):
-        return "%s is the winner and the losers are %s and %s" % (self.winner, self.losers[0], self.losers[1])
+    def __repr__(self) -> str:
+        ret: str = ""
+        columnBadge: str = ""
+        for x in range(self.columnCount):
+            if (self.columns[x] > 0):
+                columnBadge = "X"
+            else:
+                columnBadge = "_"
+            ret += "[%s]" % (columnBadge)
+        return ret
 
-    def scoreboard(self):
-        return "[%s][%s][%s]" % (self.score[0], self.score[1], self.score[2])
+    def finished(self)-> bool:
+        badges: int = sum(self.columns.values())
+        fullSheet: bool = (badges == self.columnCount)
+        return fullSheet
+
+    def play(self)-> int:
+        selection: int = random.randint(1, self.winPool)
+        amIAWinner: bool = (selection % self.winPool == 0)
+        ret: int = 0
+        if (amIAWinner):
+            pickColumn = self.columns[self.winner]
+        else:
+            pickColumn = random.choice(self.losers)
+        if (self.columns[pickColumn] == 0):
+            self.columns[pickColumn] = 1
+        else:
+            ret = 1
+        return ret
 
 
-x = group('game')
-print(x)
-print(x.scoreboard())
+game = group('game', 10, 3)
+i = 0
+keepPlaying = (not game.finished())
+while (keepPlaying):
+    # while (game.finished == False or i == 1000):
+    if(game.play()==0):
+        print(game, i)
+    i += 1
+    keepPlaying = (not game.finished() and i < 100000)
